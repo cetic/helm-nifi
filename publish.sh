1a92d3c21039454c1ac4430b6c832bb741b717ae
Alexandre Nuttinck <alexandre.nuttinck@cetic.be>
2019-06-14 15:34:22 +0200

    update nifi templates

--- publish.sh
+++ publish.sh
@@ -32,13 +32,11 @@ echo "CIRCLE_BRANCH=$CIRCLE_BRANCH"
 
 echo ">>> Create Chart Directory"
 
-mkdir $HELM_CHARTS_SOURCE/
+mkdir -p $HELM_CHARTS_SOURCE/
+mkdir -p /tmp/helm-tmp/
 
-mv $WORKING_DIRECTORY/Chart.yaml $HELM_CHARTS_SOURCE/Chart.yaml
-mv $WORKING_DIRECTORY/.helmignore $HELM_CHARTS_SOURCE/.helmignore
-mv $WORKING_DIRECTORY/LICENSE.md $HELM_CHARTS_SOURCE/LICENSE.md
-mv $WORKING_DIRECTORY/values.yaml $HELM_CHARTS_SOURCE/values.yaml
-mv $WORKING_DIRECTORY/templates $HELM_CHARTS_SOURCE/templates
+mv $WORKING_DIRECTORY/* /tmp/helm-tmp/
+mv /tmp/helm-tmp/ $HELM_CHARTS_SOURCE/
 
 echo '>> Prepare...'
 mkdir -p /tmp/helm/bin
